05e70762ee883c410c35c9239e569ad0e6444878
Toby Benedyk <tobybenedyk@gmail.com>
2013-05-28 12:53:32 +0100

    testing cleanup

--- spec/controllers/property_controller_spec.rb
+++ spec/controllers/property_controller_spec.rb
@@ -21,27 +21,4 @@ describe PropertiesController do
   		end
   	end
 
-	describe "#show" do
-		fixtures :properties
-
-		before(:all) do
-			@property = Property.first
-		end
-
-		it "should match the property" do
-			@property.should be_instance_of Property
-			Property.related(@property).should_not include(@property)
-			get :show, :id => @property.id
-			expect(response.body).to match("#{@property.name}")
-		end
-
-		it "should not be further than 20km away and have less bedrooms" do
-		  	properties = Property.related(@property)
-		  	properties.each do |related|
-		  		@property.distance_to(related).should be <= 20
-		  		@property.bedroom_count.should be <= related.bedroom_count
-		  	end
-		end
-	end
-
 end
